8624b206b8b9a96a8d45e914a1bbaec2bdc091f8
Luca Spaccatrosi <l.spaccatrosi@outlook.com>
2023-06-05 22:13:58 +0100

    why does \r\n exist?

--- src/repl/repl.v
+++ src/repl/repl.v
@@ -5,25 +5,35 @@ import parser
 import readline
 import term
 
-pub fn start() {
+fn clear_screen() {
 	term.clear()
+	println('Enter vmonkey program')
+	println('${'.clear':-10}: clears screen')
+	println('${'.exit':-10}: exit')
+	println('${'.rs':-10}: restart input')
+}
+
+pub fn start() {
+	clear_screen()
 	width, _ := term.get_terminal_size()
 	divider := '='.repeat(width)
-	println('Enter vmonkey program')
-	println('${'.rs':-10}: restart input')
-	println('${'.exit':-10}: exit')
+	
 	top: for {
 		println(divider)
 		mut ipt_lines := []string{}
 
 		for {
-			line := readline.read_line('') or { '' }
+			line := (readline.read_line('') or { '' }).replace('\r', '')
+
 			ipt_lines << line
 
 			if line == '.rs\n' {
 				continue top
 			} else if line == '.exit\n' {
 				break top
+			} else if line == '.clear\n' {
+				clear_screen()
+				continue top
 			}
 
 			if line == '' || line == '\n' {
